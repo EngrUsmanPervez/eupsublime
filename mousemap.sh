db9b12fbff6ab0c8a7f07713285c6b01b688fccc
EngrUsmanPervez <38681527+EngrUsmanPervez@users.noreply.github.com>
2023-09-19 15:24:35 +0500

    Create mousemap.sh

---
+++ mousemap.sh
@@ -0,0 +1,11 @@
+nano ~/.config/sublime-text/Packages/User/Default\ \(Linux\).sublime-mousemap
+
+[
+    {
+        "button": "button1", 
+        "count": 1, 
+        "modifiers": ["alt"],
+        "press_command": "drag_select",
+        "command": "goto_definition"
+    }
+]
