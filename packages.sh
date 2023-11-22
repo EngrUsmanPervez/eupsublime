d1d5b864096a639230977c67b4761f877af3ae7a
Arsalan Pervez <usman.pervez10@gmail.com>
2023-11-22 21:01:51 +0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 # Advanced Install Package
 
 # Utilities
-SideBarEnhancements,Alignment,BracketHighlighter,Emmet,A File Icon,SFTP,AutoFileName,Auto Semi-Colon,FileDiffs,TodoReview,ChangeQuotes,DoxyDoxygen,DocBlockr,All Autocomplete,AdvancedNewFile
+SideBarEnhancements,Alignment,BracketHighlighter,Emmet,A File Icon,SFTP,AutoFileName,Auto Semi-Colon,FileDiffs,TodoReview,ChangeQuotes,DoxyDoxygen,All Autocomplete,AdvancedNewFile
 
 # Styling
 Theme - Monokai Pro,Themr,Colorsublime,Origami
